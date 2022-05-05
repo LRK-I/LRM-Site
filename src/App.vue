46f696af816769fa2d7b47f10b600b6e896a0db5
Rin <57226653+Rinoku3@users.noreply.github.com>
2022-05-05 17:17:45 +0300

    cho nahui

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Navbar/>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <div :key="this.$route.path">
+          <component :is="Component"/>
+        </div>
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<script>
+import './styles/global.scss';
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: { Navbar },
+}
+</script>
+<style lang="scss">
+p {
+  margin-top: 0;
+  margin-bottom: 0 !important;
+}
+/* Transition style */
+.fade-enter-active, .fade-leave-active {
+  transition: all .2s ease-in-out;
+}
+.fade-enter-from, .fade-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/components/FounderComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="component">
+      <img :src="icon">
+      <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FounderComponent',
+    props: ['icon', 'name'],
+}
+</script>
+
+<style lang="scss" scoped>
+.component {
+    background: #0C0C0C;
+    height: 30px;
+    width: 200px;
+    border: 2px solid #131313;
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+    .component {
+        width: 340px;
+        margin-bottom: 10px;
+    }
+}
+</style>
